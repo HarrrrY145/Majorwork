565b86f4a475e2885633b60dbfef9c0ba3caa85d
HarrrrY145 <harry.burton1@education.nsw.gov.au>
2026-02-18 22:30:18 +1100

    #1 18/02/2026 | Setting up a virtual environment and creating a front page's HTML and CSS with a bit of functionality (Buttons to change pages)

---
+++ app.py
@@ -0,0 +1,30 @@
+print("http://localhost:8000/")
+
+# Importing  ----------------------------------------------------------------------------------------------------------------------------------------------------------------->
+
+from flask import Flask, render_template, request, redirect, session, url_for
+import sqlite3
+import os
+from waitress import serve 
+
+
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/')
+def FRONTPAGE():
+    return render_template('FRONT-PAGE.html')
+
+@app.route('/LOGIN')
+def LOGIN():
+    return render_template('LOGIN.html')
+
+@app.route('/REGISTER')
+def REGISTER():
+    return render_template('REGISTER.html')
+
+if __name__ == '__main__':
+    serve(app, host="0.0.0.0", port=8000)
+
